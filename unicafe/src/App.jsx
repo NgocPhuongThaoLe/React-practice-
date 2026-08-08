7c8d7ce25c905f0917d1601b35fbc2fe4d97ca4f
NgocPhuongThaoLe <thaole18082005@gmail.com>
2026-08-08 10:42:49 +1000

    create statistics component + refactor

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+// Statistics component 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1> statistics</h1>
+        <p> good {props.good} </p>
+        <p> neutral {props.neutral} </p>
+        <p> bad {props.bad} </p>
+        <p>all {props.all}</p>
+        <p>average {props.average}</p>
+        <p>positive {props.positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -10,20 +25,21 @@ const App = () => {
   const average = (good - bad) / all
   const positive = (good)/all
 
-
   return (
     <div>
       <h1> give feedback</h1>
         <button onClick = {() => setGood(good+1)}> good </button>
         <button onClick = {() => setNeutral(neutral+1)}> neutral </button>
         <button onClick = {() => setBad(bad+1)}> bad </button>
-      <h1> statistics</h1>
-        <p> good {good} </p>
-        <p> neutral {neutral} </p>
-        <p> bad {bad} </p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+      
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        all={all} 
+        average={average} 
+        positive={positive} 
+      />
     </div>
   )
 }
